d4c196595db02b662fa4e2933c342d84e5665474
Peter Heuer <pvheuer@gmail.com>
2022-08-01 12:41:15 -0400

    Init readme + setup

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # panoptes
- Analysis routines for HEDP imaging diagnostics
+Analysis code for HEDP imaging diagnostics
+
+# Installation Instructions
+If you intend to use this package without changing the code, follow the instructions to install as a user. If you plan on making changes to the code, install in developer mode. 
+
+## As User
+
+Run the 
+`pip install git+https://github.com/pheuer/panoptes.git`
+
+## Development Mode
+1. Clone the repository
+2. Navigate to the repository directory and run the command `python -m pip install -e .`
+
+
+## Confirming successful installation
+Try running the command 
+`import panoptes`
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='panoptes',
+    version='0.0.1',
+    author='Peter Heuer',
+    author_email='pheu@lle.rochester.edu',
+    description='Analysis code for HEDP imaging diagnostics',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/pheuer/panoptes',
+    project_urls = {
+        "Bug Tracker": "https://github.com/pheuer/panoptes/issues"
+    },
+    license='MIT',
+    packages=['cr39py'],
+    install_requires=['numpy', 'h5py', 'scipy', 'matplotlib', 'astropy'],
+)
