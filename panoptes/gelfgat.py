df2ee2569f5794236b35201badd7a4de71be175e
Peter Heuer <pvheuer@gmail.com>
2022-09-02 17:28:02 -0400

    Working on test script + gelfgat

--- panoptes/gelfgat.py
+++ panoptes/gelfgat.py
@@ -1,5 +1,70 @@
 import numpy as np
 
+import h5py
+from multiprocessing import Pool
+import tqdm
+"""
+def gelfgat_poisson(P, tmat_file, niter, h_friction=3):
+    
+    
+    with h5py.File(tmat_file, 'a') as f:
+        
+        xo = f['xo'][...]
+        yo = f['yo'][...]
+        xi = f['xi'][...]
+        yi = f['yi'][...]
+        
+        nxo, nyo = xo.size, yo.size
+        nxi, nyi = xi.size, yi.size
+        
+    # Calculate chunk size, chunking the object plane together
+    ideal_size = 10e6 #bytes
+    data_bytes = 4 # Float32
+    # Ideal chunk size in image plane (each pixel = 1 full object plane)
+    chunk_size = int(ideal_size/(nxo*nyo)/data_bytes)
+    nchunks = np.ceil(nxi*nyi/chunk_size).astype(np.int32)
+    print(f"Chunk size: {chunk_size}")
+    print(f"nchunks: {nchunks}")
+    
+    # Break the image array into chunks
+    chunks = []
+    for c in range(nchunks):
+        a = c*chunk_size
+        if c == nchunks-1:
+            b = -1
+        else:
+            b = (c+1)*chunk_size
+            
+        print(f"{a}, {b}")
+        
+        chunks.append( [P[a:b], 
+                        tmat_file, 
+                        niter, h_friction,
+                        a, b] )
+
+    solution = np.zeros([nxo*nyo, niter+1])
+    with Pool() as p:
+        
+        # Initialize a tqdm progress bar
+        with tqdm.tqdm(total=len(chunks)) as pbar:
+            
+            # Loop through the mapped calculations on the parallel pool
+            for i, result in enumerate(p.imap(_gelfgat_poisson, chunks)):
+                
+                # Store the result
+                a = chunks[i][-2]
+                b = chunks[i][-1]
+                solution[ a:b, :] = result
+                
+                # Update the progress bar that this iteration is done
+                pbar.update()
+                
+    print(solution.shape)
+    
+    return solution
+""" 
+
+
 def gelfgat_poisson(P, T, niter, h_friction=3):
     """
     Parameters
@@ -17,6 +82,7 @@ def gelfgat_poisson(P, T, niter, h_friction=3):
         will oscillate wildly and the logL can decrease.
         
     """
+    
     ishape = T.shape[0]
     oshape = T.shape[1]
 
@@ -41,38 +107,40 @@ def gelfgat_poisson(P, T, niter, h_friction=3):
     
     zeno_step = 2
     
-
-    for i in range(niter):
-        
-        Pguess = np.matmul(Tnorm, B[i,:])
-        
-        Bdelta = B[i,:] * np.matmul(Pnorm/Pguess -1, Tnorm)
-        
-        
-        # Calculate the step size
-        deltaPguess = np.matmul(Tnorm, Bdelta)
-        dLdh = np.sum(Bdelta**2 / B[i,:])
-        d2Ld2h = np.sum(Pnorm*deltaPguess**2 / Pguess**2)
-        h[i] = dLdh/d2Ld2h
-        
-        # Apply the changes
-        B[i+1,:] = B[i,:] + h[i]/h_friction*Bdelta
-        
-        
-        # Apply zenos pixel approach to prevent B from going negative
-        if zeno_step > 0:
-            B[i+1,:] = np.where(B[i+1,:] <= 0, B[i, :]/zeno_step, B[i+1,:])
+    with tqdm.tqdm(total=niter) as pbar:
+        for i in range(niter):
             
-        # Renormalize on each iteration
-        B[i+1, :] *=  1/np.sum(B[i+1, :])
-        
-        # Compute the current log likelihood 
-        
-        logL[i] = np.sum(P)*np.sum(Pnorm*np.log(Pguess))
-        
-        chisq[i] = np.sum(P)*np.sum( (Pguess - Pnorm)**2/Pguess)
-        
-        DOFs[i] = np.sum(B[i, :] / (B[i, :] + 1/B[i, :].size))
+            Pguess = np.matmul(Tnorm, B[i,:])
+            
+            Bdelta = B[i,:] * np.matmul(Pnorm/Pguess -1, Tnorm)
+            
+            
+            # Calculate the step size
+            deltaPguess = np.matmul(Tnorm, Bdelta)
+            dLdh = np.sum(Bdelta**2 / B[i,:])
+            d2Ld2h = np.sum(Pnorm*deltaPguess**2 / Pguess**2)
+            h[i] = dLdh/d2Ld2h
+            
+            # Apply the changes
+            B[i+1,:] = B[i,:] + h[i]/h_friction*Bdelta
+            
+            
+            # Apply zenos pixel approach to prevent B from going negative
+            if zeno_step > 0:
+                B[i+1,:] = np.where(B[i+1,:] <= 0, B[i, :]/zeno_step, B[i+1,:])
+                
+            # Renormalize on each iteration
+            B[i+1, :] *=  1/np.sum(B[i+1, :])
+            
+            # Compute the current log likelihood 
+            
+            logL[i] = np.sum(P)*np.sum(Pnorm*np.log(Pguess))
+            
+            chisq[i] = np.sum(P)*np.sum( (Pguess - Pnorm)**2/Pguess)
+            
+            DOFs[i] = np.sum(B[i, :] / (B[i, :] + 1/B[i, :].size))
+            
+            pbar.update()
 
         
     return B, logL, chisq, DOFs
--- panoptes/testscript.py
+++ panoptes/testscript.py
@@ -9,9 +9,9 @@ import astropy.units as u
 import os, h5py
 
 from panoptes.tmat.tmat import TransferMatrix
+from panoptes.gelfgat import gelfgat_poisson
 
-
-
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     
@@ -21,11 +21,14 @@ if __name__ == '__main__':
     data_dir = os.path.join('C:\\','Users','pheu','Data','data_dir', "103955")
 
     datafile = os.path.join(data_dir, 'xray-stack.h5')
-    tmat_path = os.path.join(data_dir, 'tmat.h5')
+    tmat_path = os.path.join(data_dir, 'tmat252.h5')
 
     psf = np.concatenate((np.ones(50), np.zeros(50)))
-    psf_ax = np.linspace(0, 2, num=100)
-
+    psf_ax = (np.linspace(0, 300, num=100)*u.um).to(u.cm).value
+    
+    
+    
+    
 
 
     with h5py.File(datafile, 'r') as f:
@@ -36,14 +39,59 @@ if __name__ == '__main__':
         pinhole_radius = f['pinholes']['pinhole_info']['diameter'][...] * u.um
         mag = f['pinholes']['mag_r'][...]
         
-    xo = np.linspace(-100, 100, num=100) * u.um
-    yo = np.linspace(-100, 100, num=100) * u.um
+        
 
+    # Thin data for testing
+    data = data[::4, ::4]
+    xi = xi[::4]
+    yi = yi[::4]
+    
+    nxi, nyi = xi.size, yi.size
+        
+    xo = (np.linspace(-100, 100, num=25) * u.um).to(u.cm)
+    yo = (np.linspace(-100, 100, num=25) * u.um).to(u.cm)
+    nxo, nyo = xo.size, yo.size
 
-    t = TransferMatrix(tmat_path)
-
-
-    print("Calculating tmat")
-    t.set_constants(xo, yo, xi, yi, mag, np.zeros([1,2])*u.cm, psf=psf, psf_ax=psf_ax)
-    t.save(tmat_path)
-    t.calc_tmat()
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal')
+    ax.pcolormesh(xi.value, yi.value, data.T)
+    plt.show()
+    
+    if not os.path.isfile(tmat_path):
+        t = TransferMatrix(tmat_path)
+    
+        print("Calculating tmat")
+        t.set_constants(xo, yo, xi, yi, mag, np.zeros([1,2])*u.cm, psf=psf, psf_ax=psf_ax)
+        t.save(tmat_path)
+        t.calc_tmat()
+        
+        
+    print("Reading tmat")  
+    with h5py.File(tmat_path, 'r') as f:
+        T = f['tmat'][...]
+        
+        
+    print(T.shape)
+    arr = np.reshape(np.sum(T, axis=0), (nxi, nyi))
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal')
+    ax.pcolormesh(xi.value, yi.value, arr.T)
+ 
+        
+    
+    # TODO: transpose the transfer matrix order in the tmat file
+    T = T.T
+    B, logL, chisq, DOFs = gelfgat_poisson(data.flatten(), T, 50, h_friction=3)
+    
+    
+    print(B.shape)
+    print(logL.shape)
+    
+    arr = np.reshape(B[40, :], (nxo, nyo))
+    
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal')
+    ax.pcolormesh(xo.value, yo.value, arr.T)
+    plt.show()
+    
+    
