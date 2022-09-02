d6262057056ae002160849c14c494405c8fc4293
Peter Heuer <pvheuer@gmail.com>
2022-09-02 16:05:54 -0400

    Update testscript.py

--- panoptes/testscript.py
+++ panoptes/testscript.py
@@ -11,42 +11,39 @@ import os, h5py
 from panoptes.tmat.tmat import TransferMatrix
 
 
-data_dir = os.path.join("C:\\","Users","pvheu","Desktop","data_dir", "103955")
-#data_dir = os.path.join('C:\\','Users','pheu','Data','data_dir', "103955")
-
-datafile = os.path.join(data_dir, 'xray-stack.h5')
-tmat_path = os.path.join(data_dir, 'tmat.h5')
-
-psf = np.concatenate((np.ones(50), np.zeros(50)))
-psf_ax = np.linspace(0, 2, num=100)
 
 
-
-with h5py.File(datafile, 'r') as f:
-    xi = f['stack']['xaxis'][...] * u.cm
-    yi = f['stack']['yaxis'][...] * u.cm
-    data = f['stack']['data'][...]
+if __name__ == '__main__':
     
-    pinhole_radius = f['pinholes']['pinhole_info']['diameter'][...] * u.um
+    #__spec__ = "ModuleSpec(name='builtins', loader=<class '_frozen_importlib.BuiltinImporter'>)"
     
-    mag = f['pinholes']['mag_r'][...]
-    
-print(mag)
+    data_dir = os.path.join("C:\\","Users","pvheu","Desktop","data_dir", "103955")
+    data_dir = os.path.join('C:\\','Users','pheu','Data','data_dir', "103955")
 
-xi = xi[0:10]
-yi = yi[0:10]
-xo = np.linspace(-100, 100, num=10) * u.um
-yo = np.linspace(-100, 100, num=10) * u.um
+    datafile = os.path.join(data_dir, 'xray-stack.h5')
+    tmat_path = os.path.join(data_dir, 'tmat.h5')
+
+    psf = np.concatenate((np.ones(50), np.zeros(50)))
+    psf_ax = np.linspace(0, 2, num=100)
 
 
-t = TransferMatrix()
-if os.path.isfile(tmat_path):
-    t.load(tmat_path)
-else:
+
+    with h5py.File(datafile, 'r') as f:
+        xi = f['stack']['xaxis'][...] * u.cm
+        yi = f['stack']['yaxis'][...] * u.cm
+        data = f['stack']['data'][...]
+
+        pinhole_radius = f['pinholes']['pinhole_info']['diameter'][...] * u.um
+        mag = f['pinholes']['mag_r'][...]
+        
+    xo = np.linspace(-100, 100, num=100) * u.um
+    yo = np.linspace(-100, 100, num=100) * u.um
+
+
+    t = TransferMatrix(tmat_path)
+
+
     print("Calculating tmat")
     t.set_constants(xo, yo, xi, yi, mag, np.zeros([1,2])*u.cm, psf=psf, psf_ax=psf_ax)
-    t.calc_tmat()
     t.save(tmat_path)
-
-
-
+    t.calc_tmat()
