f0827fc74c68dc99f4b936947f8255cc920dd45b
Peter Heuer <pvheuer@gmail.com>
2022-09-06 17:28:10 -0400

    Update testscript.py

--- panoptes/testscript.py
+++ panoptes/testscript.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     
-    oshape = (25, 25)
+    oshape = (50, 50)
     
     #__spec__ = "ModuleSpec(name='builtins', loader=<class '_frozen_importlib.BuiltinImporter'>)"
     
@@ -34,6 +34,8 @@ if __name__ == '__main__':
         R_ap = 0.5*f['pinholes']['pinhole_info']['diameter'][...] * u.um
         mag = f['pinholes']['mag_r'][...]
         
+        
+    L_det = 350 * u.cm
       
     print(f"R_ap: {R_ap}")
     print(f"Mag: {mag}")
@@ -45,23 +47,32 @@ if __name__ == '__main__':
     print(f"yi shape: {yi.shape}")
     
     
-    dxi = np.mean(np.gradient(xi))
-    dxo = (dxi/mag).to(u.um)
-    print(dxo)
+
     
-    
-    xlim = 300
+    xlim = 60
     xo = np.linspace(-xlim, xlim, num=oshape[0]) * u.um / R_ap
     yo = np.linspace(-xlim, xlim, num=oshape[1]) * u.um / R_ap
     
     xi = xi/R_ap/mag
     yi = yi/R_ap/mag
     ap_xy = np.array([[0,0],])*u.cm / R_ap
+    
+    
+   
 
     # Thin data for testing
-    data = data[::4, ::4]
-    xi = xi[::4]
-    yi = yi[::4]
+    c = 1
+    data = data[::c, ::c]
+    xi = xi[::c]
+    yi = yi[::c]
+    
+    
+    dxi = np.mean(np.gradient(xi)).to(u.dimensionless_unscaled).value
+    dxo = np.mean(np.gradient(xo)).to(u.dimensionless_unscaled).value
+    print(f"dxi: {dxi:.1e}, dxo: {dxo:.1e}")
+    
+    assert dxo > dxi, f"dxo must be greater than dxi, but {dxo}<{dxi}"
+    
     
     nxi, nyi = xi.size, yi.size
     nxo, nyo = xo.size, yo.size
@@ -76,6 +87,7 @@ if __name__ == '__main__':
 
     print("Calculating tmat")
     t.set_constants(xo, yo, xi, yi, mag, ap_xy, psf=psf, psf_ax=psf_ax)
+    t.set_dimensions(R_ap, L_det)
     t.save(tmat_path)
     calculate_tmat(t)
     
@@ -84,8 +96,9 @@ if __name__ == '__main__':
     with h5py.File(tmat_path, 'r') as f:
         T = f['tmat'][...]
 
-        
-    B, logL, chisq= gelfgat_poisson(data.flatten(), T, 50, h_friction=3)
+    
+    print(f"Running reconstruction")
+    B, logL, chisq, background= gelfgat_poisson(data.flatten(), T, 50, h_friction=3)
     
     fig, ax = plt.subplots()
     ax.plot(logL)
@@ -100,4 +113,6 @@ if __name__ == '__main__':
     fig, ax = plt.subplots()
     ax.set_title(f"Termination iter {ind+1}")
     ax.set_aspect('equal')
-    ax.pcolormesh(sol.T)
+    ax.pcolormesh(t.xo_scaled.to(u.um).value, 
+                  t.yo_scaled.to(u.um).value, 
+                  sol.T)
