583a51cdc93d4b7b08cc158353f76ee99c3759a2
Peter Heuer <pvheuer@gmail.com>
2022-09-07 14:33:23 -0400

    Update xray.py

--- panoptes/xray.py
+++ panoptes/xray.py
@@ -25,11 +25,12 @@ from matplotlib.widgets import Cursor
 
 # Make plots appear in new windows
 from IPython import get_ipython
-#get_ipython().run_line_magic('matplotlib', 'qt')
-get_ipython().run_line_magic('matplotlib', 'inline')
+
 
 
 from panoptes.pinholes import PinholeArray
+from panoptes.tmat.tmat import TransferMatrix
+from panoptes.reconstruction.gelfgat import gelfgat_poisson, GelfgatResult
 
 
 
@@ -43,6 +44,12 @@ class XrayIP:
         
             Either a shot number or a filepath directly to the data file
         """
+        
+        # Make plots appear in new windows
+        # If statement guards against running outside of ipython
+        # TODO: support plots outside of iPython...
+        if get_ipython() is not None:
+            get_ipython().run_line_magic('matplotlib', 'inline')
         
         # A figure, axis tuple for plotting
         self.figax = None
@@ -64,7 +71,17 @@ class XrayIP:
             
             
         self.shotnumber = None
+        
+        
+        self.data = None
+        self.stack = None
+        self.tmat = None
+        self.reconstruction = None
     
+    
+        if len(args) == 1:
+            self.path = args[0]
+            self.load(self.path)
             
        
             
@@ -130,8 +147,8 @@ class XrayIP:
             self.dxpx = dxpx
         
         # Arrays of the actual positions for each axis
-        self.xaxis = np.arange(nx)*self.dxpx.to(u.cm).value
-        self.yaxis = np.arange(ny)*self.dxpx.to(u.cm).value
+        self.xaxis = np.arange(nx)*self.dxpx.to(u.cm)
+        self.yaxis = np.arange(ny)*self.dxpx.to(u.cm)
         
        
         
@@ -163,64 +180,108 @@ class XrayIP:
     # Methods for loading and saving 
     # *************************************************************************
             
-    def save(self, grp):
-        """
-        Save the data about this object into an h5 group
-          
-        grp : h5py.Group or path string
-            The location to save the h5 data. This could be the root group
-            of its own h5 file, or a group within a larger h5 file.
-              
-            If a string is provided instead of a group, try to open it as
-            an h5 file and create the group there
-          
-        """
-        if isinstance(grp, str):
-            with h5py.File(grp, 'w') as f:
-                self._save(f)
-        else:
-            self._save(grp)
-        
+   
            
-    def _save(self, grp):
+    def save(self, path):
         """
         See docstring for "save"
-        """
+        """ 
+        self.path = path
         
-        # Write pinhole object
-        pinholes_grp = grp.create_group('pinholes')
-        self.pinholes.save(pinholes_grp)
+        with h5py.File(path, 'a') as grp:
+            
+            # Empty the group
+            for key in grp.keys():
+                del grp[key]
+            
+            if self.pinholes is not None:
+                # Write pinhole object
+                pinholes_grp = grp.create_group('pinholes')
+                self.pinholes.save(pinholes_grp)
+            
+            if self.data is not None:
+     
+                data_grp = grp.create_group('data')
+                data_grp['data'] = self.data
+                data_grp['xaxis'] = self.xaxis.to(u.cm).value
+                data_grp['xaxis'].attrs['unit'] = 'cm'
+                data_grp['yaxis'] = self.yaxis.to(u.cm).value
+                data_grp['yaxis'].attrs['unit'] = 'cm'
+
+      
+            
+            if self.stack is not None:
+                stack_grp = grp.create_group('stack')
+                stack_grp['data'] = self.stack
+                stack_grp['xaxis'] = self.stack_x.to(u.cm).value
+                stack_grp['xaxis'].attrs['unit'] = 'cm'
+                stack_grp['yaxis'] = self.stack_y.to(u.cm).value
+                stack_grp['yaxis'].attrs['unit'] = 'cm'
+                
+                
+            if self.tmat is not None:
+                tmat_grp = grp.create_group('tmat')
+                self.tmat.save(tmat_grp)
+                
+                
+            if self.reconstruction is not None:
+               recon_grp = grp.create_group('reconstruction')
+               self.reconstruction.save(recon_grp)
+                
+                
+                
+            
+    def load(self, path):
+        self.path = path
         
-        """
-        data_grp = grp['data']
-        data_grp['data'] = self.data
-        data_grp['xaxis'] = self.xaxis
-        data_grp['yaxis'] = self.yaxis
-        data_grp['dxpx'] = self.dxpx
-        """
+        with h5py.File(path, 'r') as f:
+            
+            if 'pinholes' in f.keys():
+                self.pinholes = PinholeArray()
+                self.pinholes.load(f['pinholes'])
+                
+            if 'data' in f.keys():
+                self.data = f['data']['data'][...]
+                self.xaxis = f['data']['xaxis'][...] * u.cm
+                self.yaxis = f['data']['yaxis'][...]* u.cm
+                
+            if 'stack' in f.keys():
+                self.stack = f['stack']['data'][...]
+                self.stack_x = f['stack']['xaxis'][...]* u.cm
+                self.stack_y = f['stack']['yaxis'][...]* u.cm
+                
+            if 'tmat' in f.keys():
+                self.tmat = TransferMatrix(f['tmat'])
+                
+            if 'reconstruction' in f.keys():
+                self.reconstruction = GelfgatResult(f['reconstruction'])
+            
         
-        stack_grp = grp.create_group('stack')
-        stack_grp['data'] = self.stack
-        stack_grp['xaxis'] = self.stack_x
-        stack_grp['yaxis'] = self.stack_y
+        
+        
         
         
 
-    def set_pinholes(self, name):
-        self.pinholes = PinholeArray(name)
+    def set_pinhole_array(self, name):
+        self.pinholes = PinholeArray()
+        self.pinholes.set_pinhole_array(name)
         
         
     def fit_pinholes(self, *args, **kwargs):
-        self.pinholes.fit(self.xaxis, self.yaxis, self.data,
+        self.pinholes.fit(self.xaxis.to(u.cm).value,
+                          self.yaxis.to(u.cm).value,
+                          self.data,
                           *args, **kwargs)
         
         
-    def stack(self):
+    def stack_pinholes(self):
         
-       sx, sy, stack =  self.pinholes.stack(self.xaxis, self.yaxis, self.data)
+       sx, sy, stack =  self.pinholes.stack(self.xaxis.to(u.cm).value,
+                                            self.yaxis.to(u.cm).value, 
+                                            self.data)
        
-       self.stack_x = sx
-       self.stack_y = sy
+       self.stack_x = sx * u.cm
+       self.stack_y = sy * u.cm
        self.stack = stack
        
        self.plot_stack()
@@ -230,7 +291,8 @@ class XrayIP:
         
         fig, ax = plt.subplots()
         ax.set_aspect('equal')
-        ax.pcolormesh(self.stack_x, self.stack_y, self.stack.T)
+        ax.pcolormesh(self.stack_x.to(u.cm).value, 
+                      self.stack_y.to(u.cm).value, self.stack.T)
         
         
     def hreflect(self):
@@ -248,7 +310,8 @@ class XrayIP:
         """
 
         # Switch to qt plotting for interactive plots
-        get_ipython().run_line_magic('matplotlib', 'qt')
+        if get_ipython() is not None:
+            get_ipython().run_line_magic('matplotlib', 'qt')
         fig, ax = self.plot()
         print("Select the data region")
         cursor = Cursor(ax, color='red', linewidth=1)
@@ -272,8 +335,8 @@ class XrayIP:
         Paramters
         ---------
         
-        subregion : list of two tuples
-            [ (xstart, ystart), (xstop, ystop)  ]
+        subregion : u.Quantity array with dimensions of length
+            [ [xstart, ystart], [xstop, ystop]  ]
             
 
         """
@@ -302,7 +365,8 @@ class XrayIP:
             fig, ax = args
             ax.clear()
         
-        x_c, y_c, data = _compressed(self.xaxis, self.yaxis, self.data)
+        x_c, y_c, data = _compressed(self.xaxis.to(u.cm).value, 
+                                     self.yaxis.to(u.cm).value, self.data)
         
         ax.set_aspect('equal')
         ax.pcolormesh(x_c, y_c, 
@@ -322,6 +386,79 @@ class XrayIP:
             
         return  fig, ax
     
+    
+    
+    def make_tmat(self, tmat_path, xyo=None, R_ap=None, L_det=350*u.cm):
+        
+            
+        if self.stack is None:
+            raise ValueError("Stack must be assembled before reconstructing")
+            
+        if R_ap is None:
+            raise ValueError("Keyword R_ap must be set.")
+            
+            
+        if xyo is None:
+            xlim = 80
+            oshape = (31,31)
+            xo = np.linspace(-xlim, xlim, num=oshape[0]) * u.um / R_ap
+            yo = np.linspace(-xlim, xlim, num=oshape[1]) * u.um / R_ap
+        else:
+            xo = xyo[0]/R_ap
+            yo = xyo[1]/R_ap
+
+        # Assume for now that we are getting a tmat for a stacked image
+        # so there is only one aperture and it is centered
+        ap_xy = np.array([[0,0],])*u.cm / R_ap
+        mag = float(self.pinholes.mag_r)
+        
+        psf = np.concatenate((np.ones(50), np.zeros(50)))
+        psf_ax = np.linspace(0, 2*R_ap, num=100)/R_ap
+        
+        xi = self.stack_x/ R_ap /mag
+        yi = self.stack_y/ R_ap / mag
+        
+        
+        c = 4
+        xi = xi[::c]
+        yi = yi[::c]
+        
+        
+        tmat = TransferMatrix(xo.to(u.dimensionless_unscaled).value,
+                              yo.to(u.dimensionless_unscaled).value,
+                              xi.to(u.dimensionless_unscaled).value,
+                              yi.to(u.dimensionless_unscaled).value,
+                              mag,
+                              ap_xy.to(u.dimensionless_unscaled).value,
+                              psf,
+                              psf_ax.to(u.dimensionless_unscaled).value,
+                              R_ap, 
+                              L_det)
+        
+        
+
+        tmat.calculate_tmat(tmat_path)
+        
+        self.tmat = tmat
+            
+
+    
+    def reconstruct(self, recon_path, tmat_pat):
+
+        print("Load tmat")
+        tmat = TransferMatrix(tmat_path)
+        self.tmat = tmat
+        
+        print("Do reconstruction")
+        
+        c=4
+        data = self.stack[::c, ::c] 
+        
+        self.reconstruction = gelfgat_poisson(data.flatten(), tmat, 50, h_friction=3)
+        
+        
+        
+    
    
     
         
@@ -332,20 +469,53 @@ if __name__ == '__main__':
     data_dir = os.path.join("C:\\","Users","pvheu","Desktop","data_dir")
     h4toh5convert_path = os.path.join('C:\\','Program Files','HDF_Group','H4H5','2.2.5','bin', 'h4toh5convert.exe')
     
-    #data_dir = os.path.join('C:\\','Users','pheu','Data','data_dir')
-    #h4toh5convert_path = os.path.join('C:\\','Program Files','HDF_Group','H4H5','2.2.5','bin', 'h4toh5convert.exe')
+    data_dir = os.path.join('C:\\','Users','pheu','Data','data_dir')
+    h4toh5convert_path = os.path.join('C:\\','Program Files','HDF_Group','H4H5','2.2.5','bin', 'h4toh5convert.exe')
     
-    obj = XrayIP( pinhole_array='D-PF-C-055_A')
     
-    obj.load_data(103955, data_dir=data_dir, h4toh5convert_path=h4toh5convert_path,)
+    save_path = os.path.join(data_dir, '103955', 'result.h5')
+    tmat_path = os.path.join(data_dir, '103955', 'tmat3.h5')
     
-    obj.set_subregion([(0.47, 9.3), (5, 0.73)])
+
+        
+       
+    obj = XrayIP()
     
-    obj.fit_pinholes(rough_adjust={'dx':-0.2, 'dy':-0.3, 'mag_s':35.5, 'rot':-17},
-                     auto_select_apertures=True,)
+    if os.path.isfile(save_path):
+        print("Loading file")
+        obj.load(save_path)
+    else:
+        obj.set_pinhole_array('D-PF-C-055_A')
+        obj.load_data(103955, data_dir=data_dir, h4toh5convert_path=h4toh5convert_path,)
     
-    obj.stack()
+        obj.set_subregion( np.array([(0.47, 9.3), (5, 0.73)])*u.cm)
     
-    save_path = os.path.join(data_dir, '103955', 'xray-stack.h5')
-    obj.save(save_path)
+        obj.fit_pinholes(rough_adjust={'dx':-0.2, 'dy':-0.3, 'mag_s':35.5, 'rot':-17},
+                         auto_select_apertures=True,)
+        
+        obj.save(save_path)
+        
+    if obj.stack is None:
+        obj.stack_pinholes()
+        obj.save(save_path)
+        
+        
+    if not os.path.isfile(tmat_path):
+        obj.make_tmat(tmat_path, R_ap=150*u.um, L_det=350*u.cm)
+        
+    if obj.reconstruction is None:
+        obj.reconstruct(save_path, tmat_path)
+        obj.save(save_path)
+        
+        
+        
+        
+    obj = XrayIP(save_path)
+    print(obj.reconstruction)
+    
+    
+    
+    
+    
+    
     
